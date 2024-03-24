e0c4ed41bcc4b0d89ba871c9e862ebeea4239f4f
Khatchi <Solomon9ice2014@gmail.com>
2024-03-24 14:43:10 +0100

    User db model created

--- api/models.py
+++ api/models.py
@@ -4,3 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    'Creates User db class'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
