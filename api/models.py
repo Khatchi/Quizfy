32e4acc3b9913611aaa460a82cda8aa5134d7643
Khatchi <Solomon9ice2014@gmail.com>
2024-03-24 15:45:16 +0100

    Question db class created

--- api/models.py
+++ api/models.py
@@ -16,3 +16,10 @@ class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     questions = db.relationship('Question', backref='quiz', lazy=True)
+
+class Question(db.Model):
+    'Creates Question db class'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(255), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    options = db.relationship('Options', backref='question', lazy=True)
