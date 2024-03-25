4d71867eec2982a85d2ee6f690b10dd1e29fe319
Khatchi <Solomon9ice2014@gmail.com>
2024-03-25 18:37:55 +0100

    initializes flask app and defined environments variables for db persistency

---
+++ api/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module handles the flask app as well as the corresponding routes
+"""
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
